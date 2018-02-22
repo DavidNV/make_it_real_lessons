25972275c68c2ba60f997dd9bdede0062947a1cc
Orferus <david.nvalderrama@gmail.com>
2018-02-22 10:04:20 -0500

    loop exercise

---
+++ loop_exercise.rb
@@ -0,0 +1,27 @@
+=begin
+Task: Please create a 4 x 4 multiplication table using loops
+
+Restrictions and expectations
+
+1. You can use as many functions or variables you want
+2. This is a common exercise so please, do not search for the answer in the internet
+3. When you're done, try to do it a in different way
+4. Do not use puts inside the build_table method
+5. The output should look like this
+
+  1 2 3 4 5
+  2 4 6 8 10
+  3 6 9 12 15
+  4 8 12 16 20
+  5 10 15 20 25
+
+=end
+
+class MultiplicationTable
+
+  def build_table
+    #You're code goes here
+  end
+end
+
+puts MultiplicationTable.new.build_table
