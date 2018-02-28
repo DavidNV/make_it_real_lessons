7a3ea0a144ed2ea65b9b3d50db714582fbd0c53b
selfeik <eleriverau@gmail.com>
2018-02-28 09:21:34 -0500

    Creé la clase actor

--- objects_class/actor.rb
+++ objects_class/actor.rb
@@ -1,5 +1,14 @@
 class Actor
-  def initialize
+  def initialize(profile, working_years, nationality, price)
+    @profile = profile
+    @working_years = working_years
+    @nationality = nationality
+    @price = price
+  end
 
+  def theatrical_knowledge
+  end
+
+  def acting_time
   end
 end
