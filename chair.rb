2a85d5669a8829d46b375f3d844be8b64a78cd8d
Trillianjose <trillianjosea@gmail.com>
2018-02-27 20:45:37 -0500

    Adding_clases

--- chair.rb
+++ chair.rb
@@ -1,20 +1,34 @@
 class Chair
+
   def initialize(material, size, style)
-    @material = material
-    @size =  size
-    @style = style
-  end
+    @material=material
+    @size=size
+    @style=style
+ end
 
-  def break
-    "Call technician"
-  end
+ def hold_weight
+   if @material=="wood" || @material=="metal"
+     if @size > 100
+       "This chair resist 150 pounds"
+     else
+       "This chair can't hold more than 120 pounds"
+     end
+   else
+     "Get a better chair"
+   end
+ end
 
-  def movement
-    if @style == "office"
-      "chair got wheels"
-    elsif  @style == "furniture"
-      "you got to lift it"
-    else
-      "this is an rooted chair"
-  end
+ def break
+   "Call a technician"
+ end
+
+ def movement
+   if @style=="office"
+     "Chair got wheels"
+   elsif @style == "furniture"
+     "You got to lift it"
+   else
+     "Get a different chair"
+   end
+ end
 end
