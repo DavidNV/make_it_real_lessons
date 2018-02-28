2494cbf3ecb9bc4bdaf79b8128eeaf2f9ebed23d
selfeik <eleriverau@gmail.com>
2018-02-28 09:13:02 -0500

    Creé clase Chair

---
+++ objects_class/chair.rb
@@ -0,0 +1,15 @@
+class Chair
+  def initialize(material, weight, height, color, year)
+    @material = material
+    @weight = weight
+    @height = height
+    @color = color
+    @year = year
+  end
+
+  def group_party_chair
+  end
+
+  def maintenance_chair
+  end
+end
