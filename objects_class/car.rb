e73231f654caebfa244631024e184fa32bb2cde2
selfeik <eleriverau@gmail.com>
2018-02-28 08:52:58 -0500

    Cree la clase car

---
+++ objects_class/car.rb
@@ -0,0 +1,14 @@
+class Car
+  def initialize(type, energy, weather, smart)
+    @type = type
+    @energy = energy
+    @weather = weather
+    @smart = smart
+  end
+
+  def drive_car
+  end
+
+  def parking_car
+  end
+end
